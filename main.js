ea0b57a7d0a07c28e5c891872a4b43c867550105
Ajaydeep Singh <deepajay0713@gmail.com>
2024-09-06 04:43:07 -0700

    Crafted the About and FAQ section + more refinements to the banner container

--- main.js
+++ main.js
@@ -19,33 +19,33 @@ const scrollRevealOption = {
 	duration: 1000,
   };
   
-  ScrollReveal().reveal(".section__container h3", {
+  ScrollReveal().reveal(".banner__container h3", {
 	...scrollRevealOption,
   });
-  ScrollReveal().reveal(".section__container h1", {
+  ScrollReveal().reveal(".banner_container h1", {
 	...scrollRevealOption,
 	origin: "left",
 	delay: 500,
   });
-  ScrollReveal().reveal(".section__container h2", {
+  ScrollReveal().reveal(".banner__container h2", {
 	...scrollRevealOption,
 	origin: "right",
 	delay: 1000,
   });
-  ScrollReveal().reveal(".section__container button", {
+  ScrollReveal().reveal(".banner__container button", {
 	...scrollRevealOption,
 	delay: 1500,
   });
-  
+  /*
   ScrollReveal().reveal(".nav__links li", {
 	...scrollRevealOption,
 	origin: "top",
 	interval: 300,
 	delay: 2000,
-  });
+  });*/
   
   ScrollReveal().reveal(".socials a", {
 	duration: 1000,
 	interval: 500,
-	delay: 4000,
+	delay: 2000,
   });
