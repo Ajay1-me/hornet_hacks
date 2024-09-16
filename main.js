32c00fdb37ba6f5a4cb03d52b5c3a540940d563f
Ajaydeep Singh <deepajay0713@gmail.com>
2024-09-15 20:04:11 -0700

    Added some more features

--- main.js
+++ main.js
@@ -57,3 +57,13 @@ const scrollRevealOption = {
     interval: 500,
     delay: 100,
   });
+
+  const root = document.documentElement;
+  const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+  const marqueeContent = document.querySelector("ul.marquee-content");
+  
+  root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+  
+  for(let i=0; i<marqueeElementsDisplayed; i++) {
+    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+  }
